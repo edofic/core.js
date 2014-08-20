3b370e6f39149da4e743eb760ec11cf25e75e6db
Andraz Bajt <edofic@gmail.com>
2014-08-20 13:50:01 +0200

    refactor main

--- src/Main.hs
+++ src/Main.hs
@@ -3,17 +3,21 @@ module Main where
 import Immediate.Simplify (simplify)
 import Immediate.Desugar (desugarModule)
 import Immediate.GenerateJs (genModule)
-import Language.Core.Parser (parse)
-import Language.Core.ParseGlue (ParseResult(OkP, FailP))
+import qualified Language.Core.Parser as P (parse) 
+import qualified Language.Core.ParseGlue as PG (ParseResult(OkP, FailP)) 
 import Language.ECMAScript3.PrettyPrint (prettyPrint)
 
 import System.Environment (getArgs)
 
+parseCore filepath = fmap p $ readFile filepath where 
+  p source = case P.parse source 0 of
+    PG.FailP err -> Left err
+    PG.OkP core  -> Right core
+
 main = do
   [filepath] <- getArgs
-  source <- readFile filepath
-  let corePR = parse source 0
-  case corePR of
-    FailP err -> putStrLn "Error parsing Core" >> putStrLn err
-    OkP core  -> print $ prettyPrint $ genModule $ simplify $ desugarModule core
+  coreE <- parseCore filepath
+  putStrLn $ either ("Error parsing Core\n"++) 
+                 (show . prettyPrint . genModule . simplify . desugarModule)
+                 coreE 
   
